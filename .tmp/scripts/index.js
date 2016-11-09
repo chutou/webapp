0b9406dcef21d9ed5af74a303bff223674de1962
zhouwy <zhouwy@landicorp.com>
2016-11-09 22:21:59 +0800

    修改最终版

--- .tmp/scripts/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
-$('#header').click(function (event) {
-	/* Act on the event */
-
-});
-//# sourceMappingURL=index.js.map
--- app/scripts/index.js
+++
@@ -1,5 +0,0 @@
-console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
-$('#header').click(function(event) {
-	/* Act on the event */
-	
-});
